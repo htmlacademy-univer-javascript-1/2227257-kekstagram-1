09c7b42bfd26f8c97a2d10fa361cb898d57e9d8e
maksim <maksim.silyukov.03@mail.ru>
2022-12-05 22:34:22 +0300

    m9t1

--- js/image-upload.js
+++ js/image-upload.js
@@ -1,34 +1,51 @@
 import { isEscapeKey } from './util.js';
 
-// Selectors for editor view
+//// Selectors for editor view
 const inputImage = document.querySelector('#upload-file');
 const overlayImage = document.querySelector('.img-upload__overlay');
 const closeButton = document.querySelector('#upload-cancel');
 
-// Selectors for hashtags and comment (description) -> Validation
+// Scale
+const buttonScaleSmaller = overlayImage.querySelector('.scale__control--smaller');
+const buttonScaleBigger = overlayImage.querySelector('.scale__control--bigger');
+const scaleControl = overlayImage.querySelector('.scale__control--value');
+const editedPicture = overlayImage.querySelector('.img-upload__preview');
+
+// Effects
+const effects = overlayImage.querySelector('.effects__list');
+const slider = overlayImage.querySelector('.effect-level__slider');
+const effectLevelInput = overlayImage.querySelector('.effect-level__value');
+const fieldSlider = overlayImage.querySelector('.img-upload__effect-level');
+let checkedBox;
+
+//// Selectors for hashtags and comment (description) -> Validation
 const form = document.querySelector('.img-upload__form');
 const hashtagInput = form.querySelector('.text__hashtags');
 const commentInput = form.querySelector('.text__description');
 const submitButton = form.querySelector('.img-upload__submit');
 
 
-// Editor view
+//// Editor view
+
 const closeOverlayImage = () => {
   inputImage.value = '';
   overlayImage.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onOverlayEscKeydown);
+  closeButton.removeEventListener('click', closeOverlayImage);
+
+  // TODO: `remove text error when upload form appears second time
+  hashtagInput.value = '';
+  commentInput.value = '';
+  submitButton.removeAttribute('disabled', 'true');
+  form.removeEventListener('submit', submitListener);
+
+  buttonScaleBigger.removeEventListener('click', changeScale);
+  buttonScaleSmaller.removeEventListener('click', changeScale);
+
+  effects.removeEventListener('change', effectPicture);
+  slider.noUiSlider.destroy();
 };
-
-inputImage.addEventListener('change', (evt) => {
-  document.addEventListener('keydown', onOverlayEscKeydown);
-
-  closeButton.addEventListener('click', closeOverlayImage);
-
-  evt.preventDefault();
-  document.body.classList.add('modal-open');
-  overlayImage.classList.remove('hidden');
-});
 
 function onOverlayEscKeydown(evt) {
   if (isEscapeKey(evt) && evt.target !== hashtagInput && evt.target !== commentInput) {
@@ -37,8 +54,141 @@ function onOverlayEscKeydown(evt) {
   }
 }
 
+function changeScale(evt) {
+  const scaleValue = scaleControl.value.replace('%', '');
+  if (evt.target === buttonScaleSmaller && scaleValue > 0) {
+    scaleControl.value = `${parseInt(scaleValue, 10) - 25}%`;
+    editedPicture.style.transform = `scale(${(parseInt(scaleValue, 10) - 25) / 100})`;
+  } else if (evt.target === buttonScaleBigger && scaleValue < 100) {
+    scaleControl.value = `${parseInt(scaleValue.replace(), 10) + 25}%`;
+    editedPicture.style.transform = `scale(${(parseInt(scaleValue, 10) + 25) / 100})`;
+  }
+}
 
-// Validation of hashtags and comment
+// Effect change
+function effectPicture(evt) {
+  checkedBox = evt.target.id;
+  let currentMin = 0;
+  let currentMax = 100;
+  let currentStart = 100;
+  let currentStep = 1;
+  switch (evt.target.id) {
+    case 'effect-chrome':
+      currentMin = 0;
+      currentMax = 1;
+      currentStep = 0.1;
+      currentStart = 1;
+      break;
+    case 'effect-sepia':
+      currentMin = 0;
+      currentMax = 1;
+      currentStep = 0.1;
+      currentStart = 1;
+      break;
+    case 'effect-marvin':
+      currentMin = 0;
+      currentMax = 100;
+      currentStep = 1;
+      currentStart = 100;
+      break;
+    case 'effect-phobos':
+      currentMin = 0;
+      currentMax = 3;
+      currentStep = 0.1;
+      currentStart = 3;
+      break;
+    case 'effect-heat':
+      currentMin = 1;
+      currentMax = 3;
+      currentStep = 0.1;
+      currentStart = 3;
+      break;
+  }
+  slider.noUiSlider.updateOptions({
+    range: {
+      min: currentMin,
+      max: currentMax
+    },
+    start: currentStart,
+    step: currentStep
+  });
+  if (evt.target.id !== 'effect-none') {
+    fieldSlider.classList.remove('hidden');
+  } else {
+    fieldSlider.classList.add('hidden');
+  }
+  // TODO: `why do we need this?
+  editedPicture.className = 'img-upload__preview';
+  const effectPreview = evt.target.parentNode.querySelector('.effects__preview');
+  editedPicture.classList.add(effectPreview.getAttribute('class').split('  ')[1]);
+  // If we use css styles, then maybe adding classes is not necessary?
+}
+
+// Effect intensity
+const effectIntens = () => {
+  const sliderValue = slider.noUiSlider.get();
+  effectLevelInput.value = sliderValue;
+  let filter;
+  switch (checkedBox) {
+    case 'effect-chrome':
+      filter = `grayscale(${sliderValue})`;
+      break;
+    case 'effect-sepia':
+      filter = `sepia(${sliderValue})`;
+      break;
+    case 'effect-marvin':
+      filter = `invert(${sliderValue}%)`;
+      break;
+    case 'effect-phobos':
+      filter = `blur(${sliderValue}px)`;
+      break;
+    case 'effect-heat':
+      filter = `brightness(${sliderValue})`;
+      break;
+  }
+  if (checkedBox === 'effect-none') {
+    editedPicture.style.filter = '';
+  } else {
+    editedPicture.style.filter = filter;
+  }
+};
+
+inputImage.addEventListener('change', (evt) => {
+  document.addEventListener('keydown', onOverlayEscKeydown);
+
+  closeButton.addEventListener('click', closeOverlayImage);
+
+  scaleControl.value = `${100}%`;
+  editedPicture.style.transform = `scale(${1})`;
+  buttonScaleSmaller.addEventListener('click', changeScale);
+  buttonScaleBigger.addEventListener('click', changeScale);
+
+  editedPicture.classList.add('effects__preview--none');
+  effects.addEventListener('change', effectPicture);
+
+  checkedBox = 'effect-none';
+  editedPicture.className = 'img-upload__preview';
+  fieldSlider.classList.add('hidden');
+  noUiSlider.create(slider, {
+    range: {
+      min: 0,
+      max: 100,
+    },
+    start: 100
+  });
+  slider.noUiSlider.on('update', () => {
+    effectIntens();
+  });
+
+  evt.preventDefault();
+  form.addEventListener('submit', submitListener);
+  document.body.classList.add('modal-open');
+  overlayImage.classList.remove('hidden');
+});
+
+
+//// Validation of hashtags and comment
+
 let boolHashtagGlobal = true;
 let boolCommentGlobal = true;
 
@@ -55,7 +205,7 @@ const pristine = new Pristine(form, {
 // If one of them doesn't match, submission (button) is disabled
 const controlSubmit = () => {
   if (!boolHashtagGlobal || !boolCommentGlobal) {
-    submitButton.setAttribute('disabled', 'false');
+    submitButton.setAttribute('disabled', 'true');
   } else {
     submitButton.removeAttribute('disabled', 'true');
   }
@@ -85,7 +235,7 @@ const validateComment = (value) => {
 pristine.addValidator(
   hashtagInput,
   validateHashtag,
-  'Хэштэг задан неправильно'
+  'Хэштег задан неправильно'
 );
 
 pristine.addValidator(
@@ -94,7 +244,7 @@ pristine.addValidator(
   'Комментарий не должен превышать 140 символов'
 );
 
-form.addEventListener('submit', (evt) => {
+function submitListener(evt) {
   evt.preventDefault();
   pristine.validate();
-});
+}
--- js/main.js
+++ js/main.js
@@ -1,25 +1,9 @@
 import { createPosts } from './data.js';
 import { renderPicture } from './render.js';
-import { openBigPicture } from './view-window.js';
 import './image-upload.js';
 
 const posts = createPosts();
 
 for (const post of posts) {
   renderPicture(post);
-}
-
-const pictures = document.querySelector('.pictures');
-
-const addCheckHandler = (post) => {
-  const srcImage = `img[src="${post.url}"]`;
-  const picture = pictures.querySelector(srcImage).parentNode;
-  picture.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    openBigPicture(post);
-  });
-};
-
-for (const post of posts) {
-  addCheckHandler(post);
 }

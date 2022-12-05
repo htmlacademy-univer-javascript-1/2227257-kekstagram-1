ca95594fca5eea2969d459628852537837ca8439
maksim <maksim.silyukov.03@mail.ru>
2022-12-05 22:44:20 +0300

    m11t1

--- js/util.js
+++ js/util.js
@@ -14,6 +14,29 @@ const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
+const showAlert = (message, alertTime) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  if (alertTime !== 0) {
+    setTimeout(() => {
+      alertContainer.remove();
+    }, alertTime);
+  }
+};
+
 checkStringLength('String', 6);
 
-export {getRandomNumber, getRandomArrayElement, isEscapeKey};
+export { getRandomNumber, getRandomArrayElement, isEscapeKey, showAlert };

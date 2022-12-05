405bea1a302e920cc0ea149d60257ef74edf897c
maksim <maksim.silyukov.03@mail.ru>
2022-12-05 22:04:13 +0300

    m8t1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {createPosts} from './data.js';
 import {renderPicture} from './render.js';
-import {openWindow, bigPicture} from './view-window.js';
+import {openBigPicture} from './view-window.js';
+import './image-upload.js';
 
 const posts = createPosts();
 
@@ -15,25 +16,10 @@ const addCheckHandler = (post) => {
   const picture = pictures.querySelector(srcImage).parentNode;
   picture.addEventListener('click', (evt) => {
     evt.preventDefault();
-    document.body.classList.add('modal-open');
-    openWindow(post);
+    openBigPicture(post);
   });
 };
 
 for (const post of posts) {
   addCheckHandler(post);
 }
-
-const closeButton = bigPicture.querySelector('.big-picture__cancel');
-
-closeButton.addEventListener('click', () => {
-  bigPicture.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-});
-
-document.addEventListener('keydown', (evt) => {
-  if (evt.key === 'Escape') {
-    bigPicture.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-  }
-});

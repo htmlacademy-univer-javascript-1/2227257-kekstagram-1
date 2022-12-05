2e328b7e5788dd7236264d7c5f1253b5dea110fd
maksim <maksim.silyukov.03@mail.ru>
2022-12-05 22:52:55 +0300

    m12t2

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import { rendering } from './render.js';
 import { getData } from './api.js';
 import { showAlert } from './util.js';
 import './image-upload.js';
+import './preview-file.js';
 
 getData((posts) => {
   rendering(posts);}, () => {
